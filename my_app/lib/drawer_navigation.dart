08e628e314a1a0cda6135ec589dec99aa46b08e1
doubleOZ <monsterdaniel97@gmail.com>
2021-08-13 16:57:43 +0800

    added faq tab into app drawer

--- my_app/lib/drawer_navigation.dart
+++ my_app/lib/drawer_navigation.dart
@@ -11,17 +11,11 @@ class MyDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           const DrawerHeader(
-            decoration: BoxDecoration(
-                color: Colors.orange
-            ),
+            decoration: BoxDecoration(color: Colors.orange),
             child: Text("View"),
           ),
           ListTile(
-            leading: Icon(
-                Icons.home,
-                color: Colors.pinkAccent,
-                size: 24.0),
-
+            leading: Icon(Icons.home, color: Colors.pinkAccent, size: 24.0),
             title: const Text("Home"),
             onTap: () {
               Navigator.pop(context);
@@ -30,24 +24,18 @@ class MyDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(
-                Icons.notifications,
-                color: Colors.pinkAccent,
-                size: 24.0),
+            leading:
+                Icon(Icons.notifications, color: Colors.pinkAccent, size: 24.0),
             title: const Text('Notification'),
             onTap: () {
               // Update the state of the app
               // ...
               // Then close the drawer
               Navigator.pop(context);
-
             },
           ),
           ListTile(
-            leading: Icon(
-                Icons.email,
-                color: Colors.pinkAccent,
-                size: 22.0),
+            leading: Icon(Icons.email, color: Colors.pinkAccent, size: 22.0),
             title: const Text('Request'),
             onTap: () {
               // Update the state of the app
@@ -57,10 +45,8 @@ class MyDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(
-                Icons.calendar_today_outlined,
-                color: Colors.pinkAccent,
-                size: 22.0),
+            leading: Icon(Icons.calendar_today_outlined,
+                color: Colors.pinkAccent, size: 22.0),
             title: const Text('Leaves'),
             onTap: () {
               // Update the state of the app
@@ -70,10 +56,8 @@ class MyDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(
-                Icons.assessment_outlined,
-                color: Colors.pinkAccent,
-                size: 24.0),
+            leading: Icon(Icons.assessment_outlined,
+                color: Colors.pinkAccent, size: 24.0),
             title: const Text('Work statistics'),
             onTap: () {
               // Update the state of the app
@@ -82,14 +66,11 @@ class MyDrawer extends StatelessWidget {
               Navigator.pop(context);
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (BuildContext context) => Stats()));
-
             },
           ),
           ListTile(
-            leading: Icon(
-                Icons.announcement_outlined,
-                color: Colors.pinkAccent,
-                size: 24.0),
+            leading: Icon(Icons.announcement_outlined,
+                color: Colors.pinkAccent, size: 24.0),
             title: const Text('Announcement'),
             onTap: () {
               // Update the state of the app
@@ -99,10 +80,14 @@ class MyDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(
-                Icons.logout,
-                color: Colors.pinkAccent,
-                size: 24.0),
+              leading: Icon(Icons.help, color: Colors.white, size: 24.0),
+              title: const Text('FAQ'),
+              onTap: () {
+                //TODO add functionality to bring up a faq popup
+                Navigator.pop(context);
+              }),
+          ListTile(
+            leading: Icon(Icons.logout, color: Colors.pinkAccent, size: 24.0),
             title: const Text('Log Out'),
             onTap: () {
               // Update the state of the app
@@ -111,7 +96,6 @@ class MyDrawer extends StatelessWidget {
               Navigator.pop(context);
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (BuildContext context) => LoginScreen()));
-
             },
           ),
         ],
