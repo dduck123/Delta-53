c81e051b63f6342477316c3a27f1f70feae45123
mvaeve <Maeveng25@gmail.com>
2021-08-19 18:53:29 +0800

    dididada

--- my_app/lib/pages/announcement_page.dart
+++ my_app/lib/pages/announcement_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_app/helper/drawer_navigation.dart';
+import 'dart:math';
 
 class Announce extends StatefulWidget {
   const Announce({Key? key}) : super(key: key);
@@ -12,20 +14,15 @@ class Announce extends StatefulWidget {
 class _AnnounceState extends State<Announce> {
   final textControllerTitle = TextEditingController();
   final textControllerMessage = TextEditingController();
+  var textTitle = TextEditingController();
+  var textMessage = TextEditingController();
+  final _random = Random();
+
   CollectionReference announcements =
-  FirebaseFirestore.instance.collection('Announcements');
+      FirebaseFirestore.instance.collection('Announcements');
 
   @override
-
   Widget build(BuildContext context) {
-
-    var size = MediaQuery
-        .of(context)
-        .size;
-
-    /*24 is for notification bar on Android*/
-    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
-    final double itemWidth = size.width / 2;
 
     return MaterialApp(
       home: Scaffold(
@@ -38,53 +35,103 @@ class _AnnounceState extends State<Announce> {
             if (!snapshot.hasData) {
               return Center(child: Text("Loading..."));
             }
-            return GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: (itemWidth / itemHeight),
+            return ListView(
+              padding: const EdgeInsets.all(20),
               children: snapshot.data!.docs.map((announce) {
-                return Center(
-                  child: ListTile(
-                    title: Text(announce['title']),
-                    subtitle: Text(announce['message']),
-                    trailing: PopupMenuButton(
-                      itemBuilder: (context){
+                return Card(
+                  elevation: 10.0,
+                  shadowColor: Colors.purpleAccent,
+                  margin: EdgeInsets.symmetric(horizontal: 80.0, vertical: 10.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  child: Container(
+
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      tileColor: Colors.primaries[_random.nextInt(Colors.primaries.length)]
+                [_random.nextInt(4) * 100],
+                      contentPadding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      title: Text(announce['title']),
+                      subtitle: Text(announce['message']),
+                      trailing: PopupMenuButton(itemBuilder: (context) {
                         return [
                           PopupMenuItem(
-                              value: "Edit",
-                              child: Text("Edit"),
+                            value: "Edit",
+                            child: Text("Edit"),
                           ),
                           PopupMenuItem(
                             value: "Delete",
                             child: Text("Delete"),
                           )
                         ];
-                      },
-                      onSelected: (String value){
-                        if (value == "Delete"){
+                      }, onSelected: (String value) {
+                        if (value == "Edit") {
+                          textTitle.text = announce["title"];
+                          textMessage.text = announce["message"];
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(40)),
+                                  elevation: 16,
+                                  child: Container(
+                                    child: ListView(
+                                      shrinkWrap: true,
+                                      children: <Widget>[
+                                        SizedBox(height: 20),
+                                        Center(
+                                            child: Text('Update announcement')),
+                                        SizedBox(height: 20),
+                                        TextField(
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            contentPadding:
+                                                EdgeInsets.only(top: 14.0),
+                                            labelText: 'Title',
+                                          ),
+                                          controller: textTitle,
+                                        ),
+                                        TextField(
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            contentPadding:
+                                                EdgeInsets.only(top: 14.0),
+                                            labelText: 'Message',
+                                          ),
+                                          controller: textMessage,
+                                        ),
+                                        RaisedButton(
+                                          child: Text("Update"),
+                                          onPressed: () {
+                                            announce.reference.update({
+                                              'title': textTitle.text,
+                                              'message': textMessage.text,
+                                            });
+
+                                            Navigator.pop(
+                                                context, announcements);
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              });
+                        }
+                        if (value == "Delete") {
                           announce.reference.delete();
                         }
-                      }
+                      }),
                     ),
-
-
                   ),
                 );
               }).toList(),
             );
-            // return ListView(
-            //   children: snapshot.data!.docs.map((announce) {
-            //     return Center(
-            //       child: ListTile(
-            //         title: Text(announce['title']),
-            //         subtitle: Text(announce['message']),
-            //         trailing: Icon(Icons.more_vert),
-            //         onLongPress: () {
-            //           announce.reference.delete();
-            //         },
-            //       ),
-            //     );
-            //   }).toList(),
-            // );
           },
         ),
         floatingActionButton: FloatingActionButton(
@@ -140,9 +187,8 @@ class _AnnounceState extends State<Announce> {
             );
           },
         ),
+        drawer: MyDrawer(),
       ),
     );
   }
 }
-
-
