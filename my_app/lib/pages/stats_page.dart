f2f9ab36721dceac3be168126cc008d082ec3fdd
Dan Miller <monsterdaniel97@gmail.com>
2021-08-27 15:09:09 +0800

    slight more changes to the layout of stats.dart

--- my_app/lib/pages/stats_page.dart
+++ my_app/lib/pages/stats_page.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/helper/drawer_navigation.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Stats extends StatelessWidget {
   const Stats({Key? key}) : super(key: key);
@@ -96,7 +99,7 @@ class _LeavesState extends State<Leaves> {
         child: Column(
       //set all children within to line up vertically
       mainAxisAlignment: MainAxisAlignment
-          .start, //starting point for the first row in this column
+          .spaceEvenly, //starting point for the first row in this column
       children: <Widget>[
         Container(
           color: Colors.grey[350],
@@ -109,7 +112,8 @@ class _LeavesState extends State<Leaves> {
                 radius: 120.0,
                 lineWidth: 15.0,
                 percent: 0.8,
-                center: new Text('24/30'),
+                center: new Text('24/30', style: TextStyle(fontSize: 17)),
+                //TODO link this up to database somehow
                 progressColor: Colors.tealAccent[400],
                 backgroundColor: Colors.grey[500],
               ),
@@ -121,6 +125,11 @@ class _LeavesState extends State<Leaves> {
                   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
             ],
           ),
+        ),
+        Divider(
+          thickness: 3,
+          indent: 20,
+          endIndent: 20,
         ),
         //new row Attendance related widgets
         Container(
@@ -139,36 +148,41 @@ class _LeavesState extends State<Leaves> {
               CircularPercentIndicator(
                 radius: 120.0,
                 lineWidth: 15.0,
-                percent: 0.8,
-                center: new Text('24/30'),
+                percent: 0.88,
+                center: new Text('88%', style: TextStyle(fontSize: 17)),
+                //TODO link this up to database somehow
                 progressColor: Colors.red[200],
                 backgroundColor: Colors.grey[500],
               ),
             ],
           ),
         ),
+        Divider(
+          thickness: 3,
+          indent: 20,
+          endIndent: 20,
+        ),
         //Payslip row
-        Card(
+        Container(
+            height: 200,
+            color: Colors.grey[350],
             child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              child: ListTile(
-                title: Text(
-                  'Payslip',
-                  style: TextStyle(fontSize: 50),
-                  textAlign: TextAlign.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ListTile(
+                  title: Text(
+                    'Payslip',
+                    style: TextStyle(fontSize: 50),
+                    textAlign: TextAlign.center,
+                  ),
+                  subtitle: Text(
+                    '\$4000',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 50),
+                  ),
                 ),
-                subtitle: Text(
-                  '\$4000',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-              color: Colors.grey[350],
-            ),
-          ],
-        )),
+              ],
+            )),
       ],
     ));
   }
