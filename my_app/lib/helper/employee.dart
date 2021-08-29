4298c3f01598e0e34c635b28c9e32714182e0f18
Maeve Ng <maeve@Maeves-MacBook-Pro.local>
2021-08-29 16:04:32 +0800

    design personal data

--- my_app/lib/helper/employee.dart
+++ my_app/lib/helper/employee.dart
@@ -17,37 +17,126 @@ class _EmployeeState extends State<Employee> {
     CollectionReference employees =
         FirebaseFirestore.instance.collection('Employees');
     return Scaffold(
-        body: Center(
-            child: FutureBuilder<DocumentSnapshot>(
-          future: employees.doc(currentUserID).get(),
-          builder:
-              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              Map<String, dynamic> data =
-                  snapshot.data!.data() as Map<String, dynamic>;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "User ID: " + currentUserID,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w200,
-                      fontSize: 24,
-                    ),
+        body: Column(children: <Widget>[
+      Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Colors.indigoAccent,
+                Colors.indigo,
+                Colors.white
+              ])),
+          child: Container(
+            width: double.infinity,
+            height: 350.0,
+            child: Center(
+              child: FutureBuilder<DocumentSnapshot>(
+                  future: employees.doc(currentUserID).get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      Map<String, dynamic> data =
+                          snapshot.data!.data() as Map<String, dynamic>;
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              "https://actnowtraining.files.wordpress.com/2012/02/cat.jpg",
+                            ),
+                            radius: 50.0,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            "${data['name']} ${data['surname']}",
+                            style: TextStyle(
+                              fontSize: 30.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Card(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 4.0),
+                            clipBehavior: Clip.antiAlias,
+                            color: Colors.white,
+                            shadowColor: Colors.black,
+                            elevation: 8.0,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0, vertical: 22.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Column(
+                                      children: <Widget>[
+                                        Text(
+                                          "Position",
+                                          style: TextStyle(
+                                            color: Colors.indigo,
+                                            fontSize: 24.0,
 
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 5.0,
+                                        ),
+                                        Text(
+                                          "${data['position']}",
+                                          style: TextStyle(
+                                            fontSize: 28.0,
+                                            color: Colors.redAccent,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }
+                    return Text("loading...");
+                  }),
+            ),
+          )),
+      Center(
+          child: FutureBuilder<DocumentSnapshot>(
+        future: employees.doc(currentUserID).get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "User ID: " + currentUserID,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200,
+                    fontSize: 24,
                   ),
-                  SizedBox(height: 10),
-                  Text("Full Name: ${data['name']} ${data['surname']}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w200,
-                      fontSize: 24,
-                    ),)
-                ],
-              );
-            }
+                ),
+                SizedBox(height: 10),
+              ],
+            );
+          }
 
-            return Text("loading...");
-          },
-        )));
+          return Text("loading...");
+        },
+      ))
+    ]));
   }
 }
