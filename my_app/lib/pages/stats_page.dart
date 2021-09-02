6acbf13182b0be9e07ee75753f00f462f3787c16
Dan Miller <monsterdaniel97@gmail.com>
2021-09-03 00:41:31 +0800

    completed the payslip, leaves and attendance linked with database

--- my_app/lib/pages/stats_page.dart
+++ my_app/lib/pages/stats_page.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/helper/drawer_navigation.dart';
+
 import 'package:my_app/helper/employee.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Stats extends StatelessWidget {
   const Stats({Key? key}) : super(key: key);
@@ -99,109 +102,114 @@ class _LeavesState extends State<Leaves> {
   Widget build(BuildContext context) {
     CollectionReference employees =
         FirebaseFirestore.instance.collection('Employees');
-    return Container(
-        child: Column(
-      //set all children within to line up vertically
-      mainAxisAlignment: MainAxisAlignment
-          .spaceEvenly, //starting point for the first row in this column
-      children: [
-        Center(
-            child: FutureBuilder<DocumentSnapshot>(
-                future: employees.doc(currentUserID).get(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<DocumentSnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    Map<String, dynamic> data =
-                        snapshot.data!.data() as Map<String, dynamic>;
-                    Container(
-                      color: Colors.grey[350],
+    return Scaffold(
+        body: Container(
+            child: Center(
+      child: FutureBuilder<DocumentSnapshot>(
+          future: employees.doc(currentUserID).get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
+
+              return ListView(
+                children: <Widget>[
+                  Container(
+                    color: Colors.grey[350],
+                    height: 200,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircularPercentIndicator(
+                          radius: 120.0,
+                          lineWidth: 15.0,
+                          percent: (data['leaves'] / 30),
+                          center: new Text('${data['leaves']}/30',
+                              style: TextStyle(fontSize: 17)),
+                          progressColor: Colors.tealAccent[400],
+                          backgroundColor: Colors.grey[500],
+                        ),
+                        Padding(
+                          //the spacing between Text and Percent indicator
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                        Text('LEAVES',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.bold)),
+                        Padding(
+                          //the spacing between Text and Percent indicator
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 3,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  Container(
+                    color: Colors.grey[350],
+                    height: 200,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('ATTENDANCE',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.bold)),
+                        Padding(
+                          //the spacing between Text and Percent indicator
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                        CircularPercentIndicator(
+                          radius: 120.0,
+                          lineWidth: 15.0,
+                          percent: (data['attendance'] / 365),
+                          center: new Text(
+                              ((data['attendance'] / 365 * 100)
+                                      .toStringAsFixed(2) +
+                                  "%"),
+                              style: TextStyle(fontSize: 17)),
+                          //TODO link this up to database somehow
+                          progressColor: Colors.red[200],
+                          backgroundColor: Colors.grey[500],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 3,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  Container(
                       height: 200,
-                      width: double.infinity,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularPercentIndicator(
-                            radius: 120.0,
-                            lineWidth: 15.0,
-                            percent: 0.8,
-                            center: new Text('24/30',
-                                style: TextStyle(fontSize: 17)),
-                            //TODO link this up to database somehow
-                            progressColor: Colors.tealAccent[400],
-                            backgroundColor: Colors.grey[500],
-                          ),
-                          Padding(
-                            //the spacing between Text and Percent indicator
-                            padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          ),
-                          Text('LEAVES',
-                              style: TextStyle(
-                                  fontSize: 40, fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                    );
-                    Divider(
-                      thickness: 3,
-                      indent: 20,
-                      endIndent: 20,
-                    );
-                    //new row Attendance related widgets
-                    Container(
                       color: Colors.grey[350],
-                      height: 200,
-                      width: double.infinity,
-                      child: Row(
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text('ATTENDANCE',
-                              style: TextStyle(
-                                  fontSize: 40, fontWeight: FontWeight.bold)),
-                          Padding(
-                            //the spacing between Text and Percent indicator
-                            padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          ),
-                          CircularPercentIndicator(
-                            radius: 120.0,
-                            lineWidth: 15.0,
-                            percent: 0.88,
-                            center:
-                                new Text('88%', style: TextStyle(fontSize: 17)),
-                            progressColor: Colors.red[200],
-                            backgroundColor: Colors.grey[500],
-                          ),
-                        ],
-                      ),
-                    );
-                    Divider(
-                      thickness: 3,
-                      indent: 20,
-                      endIndent: 20,
-                    );
-                    //Payslip row
-                    Container(
-                        height: 200,
-                        color: Colors.grey[350],
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ListTile(
-                              title: Text(
-                                'Payslip',
-                                style: TextStyle(fontSize: 50),
-                                textAlign: TextAlign.center,
-                              ),
-                              subtitle: Text(
-                                "${data['name']}",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 50),
-                              ),
+                        children: [
+                          ListTile(
+                            title: Text(
+                              'Payslip',
+                              style: TextStyle(fontSize: 50),
+                              textAlign: TextAlign.center,
                             ),
-                          ],
-                        ));
-                  }
-                  return Text('loading');
-                })),
-      ],
-    ));
+                            subtitle: Text(
+                              '\$${data['payslip']}',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 50),
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
+              );
+            }
+            return Text("loading...");
+          }),
+    )));
   }
 }
